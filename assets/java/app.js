25e96d17eb57e6b57bc80544930acac71b4d5440
james inch <james.inch0103@gmail.com>
2018-08-11 13:06:05 -0400

    test ajax

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,7 +1,7 @@
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://api.themoviedb.org/3/movie/latest?api_key=aa7ab589539224ab4b6e6c3101db8e4a&language=en-US&include_adult=false",
+    "url": "https://api.themoviedb.org/3/movie/popular?api_key=aa7ab589539224ab4b6e6c3101db8e4a&language=en-US&page=1",
     "method": "GET",
     "headers": {},
     "data": "{}"
