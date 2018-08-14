d8998a4f42716960c0c53e5b961feee488dd0819
james inch <james.inch0103@gmail.com>
2018-08-13 20:44:43 -0400

    null

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,12 +1,26 @@
 var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.themoviedb.org/3/movie/popular?api_key=aa7ab589539224ab4b6e6c3101db8e4a&language=en-US&page=1",
-    "method": "GET",
-    "headers": {},
-    "data": "{}"
-  }
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.themoviedb.org/3/movie/popular?api_key=aa7ab589539224ab4b6e6c3101db8e4a&language=en-US&page=1",
+  "method": "GET",
+  "headers": {},
+  "data": "{page:1}"
+}
+
+$.ajax(settings).done(function (response) {
+
+  var results = response.results
+  console.log(results);
+
+  for( var i = 0; i < 5; i++){
+    // console.log(results[i].title);
+
+    var title = results[i].title
+    console.log(title + 0);
+
+    
+
+
+  };
+
+});
