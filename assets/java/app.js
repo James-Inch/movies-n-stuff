b9dc925eddecb88a283ad3ffe6e6b4e9d2affbdc
james inch <james.inch0103@gmail.com>
2018-08-17 17:01:28 -0400

    add comment page

--- assets/java/app.js
+++ assets/java/app.js
@@ -36,4 +36,53 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
+$("#reviewSubmit").on("click", function (event) {
+  event.preventDefault();
+
+  var email = $("#email").val().trim();
+  var name = $("#userName").val().trim();
+  var title = $("#movieTitle").val().trim();
+  var review = $("#userReview").val().trim();
+
+  var reviewObj = {
+    email: email,
+    name: name,
+    title: title,
+    review: review
+  }
+
+  database.ref().push(reviewObj);
+
+  $("#email").val("");
+  $("#userName").val("");
+  $("#movieTitle").val("");
+  $("#userReview").val("");
+
+
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+
+  var name = childSnapshot.val().name;
+  var title = childSnapshot.val().title;
+  var review = childSnapshot.val().review;
+
+  var reviewDiv = $("<div class='jumbotron' style='padding: 32px; border-radius: 50px; color: rgb(63, 231, 253)'>");
+
+  var movieReviewed = $("<h1>").text(title);
+
+  var p = $("<p>").text(review);
+
+  var whosReview = $("<h5>").text("- " + name);
+  
+  reviewDiv.append(movieReviewed);
+
+  reviewDiv.append(p);
+  reviewDiv.append(whosReview);
+
+  $("#reviews-go-here").append(reviewDiv);
+});
+
 
